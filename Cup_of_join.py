f847c8da4d176941910939e27d54d2ce82671463
amitai shmeeda <73076329+amitai007@users.noreply.github.com>
2023-04-30 18:01:43 +0300

    changes made
    1. main
    2. pep8

--- Cup_of_join.py
+++ Cup_of_join.py
@@ -1,6 +1,10 @@
 # region Cup of join
 
-def Cup_of_join(*args, sep='-'):
+def cup_of_join(*args, sep='-'):
+    """Accepts an unlimited number of lists.
+    and parameter sep between any two lists, if not provided, the character "-" 
+    returns a list consisting of lists
+    """
     result = []
     if len(args) == 0:
         return None
@@ -12,3 +16,10 @@ def Cup_of_join(*args, sep='-'):
 
 
 # endregion
+
+if __name__ == '__main__':
+    
+    cup_of_join([1, 2], [8], [9, 5, 6], sep='@')
+    cup_of_join([1, 2], [8], [9, 5, 6])
+    cup_of_join([1])
+    cup_of_join()
