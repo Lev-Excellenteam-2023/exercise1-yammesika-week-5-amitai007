8b7224f04607e3bc93cade11ad8252acf915a6f9
amitai shmeeda <73076329+amitai007@users.noreply.github.com>
2023-05-01 10:19:22 +0300

    update for dont_have_vinegar
    1) add main
    2) seprate IO from logic
    3) without  try, except

--- dont_have_vinegar.py
+++ dont_have_vinegar.py
@@ -3,41 +3,46 @@ import calendar
 import datetime
 import random
 
+def check_dates(first_date, second_date):
+    # Check if first_date is not greater than second_date
+    if second_date > first_date:
+        return True
+    else:
+        return False
+    
 
-def generate_date():
+def generate_date(first_date, second_date):
     """The user enters two dates in the format YYYY-MM-DD.
     The program generates a random date between the two dates."""
-    # Get two dates from the user input
-    while True:
-        try:
-            date1 = input("Enter the first date in YYYY-MM-DD format: ")
-            date1 = datetime.datetime.strptime(date1, '%Y-%m-%d').date()
-            break
-        except ValueError:
-            print("Invalid input. Please try again.")
-
-    while True:
-        try:
-            date2 = input("Enter the second date in YYYY-MM-DD format: ")
-            date2 = datetime.datetime.strptime(date2, '%Y-%m-%d').date()
-            # Check if date1 is not greater than date2
-            if date2 > date1:
-                break
-            else:
-                print("The second date must be later than the first date. Please try again.")
-        except ValueError:
-            print("Invalid input. Please try again.")
-
     # Generate a random date between the two dates
-    delta = date2 - date1
+    delta = second_date - first_date
     random_days = datetime.timedelta(days=random.randint(0, delta.days))
-    new_date = date1 + random_days
+    new_date = first_date + random_days
 
     # Check if the new date is on a Monday
     if calendar.day_name[new_date.weekday()] == 'Monday':
-        print(f"The new date is {new_date.strftime('%Y-%m-%d')}" + " I have no vinegar!")
+        return f"The new date is {new_date.strftime('%Y-%m-%d')}" + " I have no vinegar!"
     else:
-        print(f"The new date is {new_date.strftime('%Y-%m-%d')}")
+        return f"The new date is {new_date.strftime('%Y-%m-%d')}"
 
 
 # endregion
+
+
+if __name__ == '__main__':
+    
+    # Get two dates from the user input
+    first_date = input("Enter the first date in YYYY-MM-DD format: ")
+    first_date = datetime.datetime.strptime(first_date, '%Y-%m-%d').date()
+    second_date = input("Enter the second date in YYYY-MM-DD format: ")
+    second_date = datetime.datetime.strptime(second_date, '%Y-%m-%d').date()
+    
+    # Check if first_date is not greater than second_date 
+    flag = check_dates(first_date, second_date)
+    if flag == True:
+        generate_date(first_date, second_date)
+    else:
+        print("The second date must be later than the first date. Please try again.")
+    
+    
+       
