76ffe9847526d992d39d23b04f1b0608855afc7d
amitai shmeeda <73076329+amitai007@users.noreply.github.com>
2023-05-01 10:35:32 +0300

    Update
    1) main
    2)  documentation
    3) iterable in iterables

--- interleave.py
+++ interleave.py
@@ -1,24 +1,37 @@
-# import the cycle and islice functions from the itertools module
 from itertools import cycle, islice
 
 
-# define a function called round_robin that accepts any number of iterables
 def round_robin(*iterables):
-    """round_robin('ABC', 'D', 'EF') --> A D E B F C"""
-    # get the number of iterables passed in
+    """
+    Take any number of iterables and return their elements in a round-robin fashion.
+    Args:
+    *iterables: any number of iterables (e.g. lists, tuples, etc.)
+    Yields:
+    The next value from each iterable in a round-robin fashion.
+    Example:
+    round_robin('ABC', 'D', 'EF') -> A D E B F C
+    """
     num_active = len(iterables)
 
     # create a cycle object that iterates over the __next__ method of each iterable
-    nexts = cycle(iter(it).__next__ for it in iterables)
+    nexts = cycle(iter(iterable).__next__ for iterable in iterables)
 
     # loop until there are no more active iterables
     while num_active:
         try:
-            # iterate over the next method of each iterable in the cycle
             for next in nexts:
-                # yield the next value from the iterable
                 yield next()
         except StopIteration:
             # if an iterable raises StopIteration, remove it from the cycle
             num_active -= 1
             nexts = cycle(islice(nexts, num_active))
+
+
+def main():
+    # example
+    for item in round_robin('ABC', 'D', 'EF'):
+        print(item)
+
+
+if __name__ == '__main__':
+    main()
